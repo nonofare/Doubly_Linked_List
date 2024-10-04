ccfe4ee0009fd00186139f080f66e978ef44fc2c
nonofare <adik96885@gmail.com>
2024-10-04 23:54:45 +0200

    Added some list methods

--- Doubly_Linked_List/DLL.h
+++ Doubly_Linked_List/DLL.h
@@ -1,54 +1,149 @@
 namespace DLL {
-	
+
 	template <typename T>
 	class List {
+
+		template <typename T>
+		class Node {
+			friend List;
+
+		private:
+			T data;
+			Node<T>* next;
+			Node<T>* prev;
+
+		public:
+			Node(T data) {
+				this->data = data;
+				next = nullptr;
+				prev = nullptr;
+			}
+		};
+
+	private:
 		size_t size;
-		ListNode<T>* head;
-		ListNode<T>* tail;
+		Node<T>* head;
+		Node<T>* tail;
 
 	public:
-		List<T> List<T>() {
+		List() {
 			size = 0;
+			head = nullptr;
+			tail = nullptr;
 		}
 
+		// Dodawanie nowego elementu na poczatku listy (2b)
 		void add_at_head(T data) {
-			ListNode<T>* node = new ListNode<T>(data);
+			Node<T>* newNode = new Node<T>(data);
 			if (size > 0) {
-				head->prev = node;
-				node->next = head;
-				head = node;
+				head->prev = newNode;
+				newNode->next = head;
+				head = newNode;
 			}
 			else {
-				head = node;
-				tail = node;
+				head = newNode;
+				tail = newNode;
 			}
 			size++;
 		}
 
+		// Dodawanie nowego elementu na koncu listy (2a)
 		void add_at_tail(T data) {
-			ListNode<T>* node = new ListNode<T>(data);
+			Node<T>* newNode = new Node<T>(data);
 			if (size > 0) {
-				tail->next = node;
-				node->prev = tail;
-				tail = node;
+				tail->next = newNode;
+				newNode->prev = tail;
+				tail = newNode;
 			}
 			else {
-				head = node;
-				tail = node;
+				head = newNode;
+				tail = newNode;
 			}
 			size++;
 		}
-	};
 
-	template <typename T>
-	class ListNode {
-		T data;
-		ListNode<T>* next;
-		ListNode<T>* prev;
+		// Usiniecie pierwszego elementu (2d)
+		bool remove_at_head() {
+			if (size > 1) {
+				Node<T>* temp = head->next;
+				delete head;
+				head = temp;
+				size--;
 
-	public:
-		ListNode<T> ListNode<T>(T data) {
-			this.data = data;
+				return true;
+			}
+			else if (size == 1) {
+				delete head;
+				head = tail = nullptr;
+				size--;
+
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		// Usiniecie ostatniego elementu (2c)
+		bool remove_at_tail() {
+			if (size > 1) {
+				Node<T>* temp = tail->prev;
+				delete tail;
+				tail = temp;
+				size--;
+
+				return true;
+			}
+			else if (size == 1) {
+				delete tail;
+				head = tail = nullptr;
+				size--;
+
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		// Zwrocenie danych i-tego elementu listy (2e)
+		bool get_data_by_index(size_t index) {
+			if (size < index + 1 || index < 0) {
+				return false;
+			}
+			else {
+				
+			}
+		}
+
+		// Ustawienie danych i-tego elementu listy(2f)
+		void set_data_by_index(T data) {
+
+		}
+
+		// Wyszukiwanie elementu (2g)
+		void find() {
+
+		}
+
+		// Wyszukiwanie i usiniecie elementu (2h)
+		void find_and_delete() {
+
+		}
+
+		// Dodanie nowego elementu z wymuszeniem porzadku (2i)
+		void idk() {
+
+		}
+
+		// Usuniecie wszystkich elementow listy (2j)
+		void erase() {
+
+		}
+
+		// Zwrocenie napisowej reprezentacji listy (2k) (XD?)
+		void idk2() {
+
 		}
 	};
 }
--- Doubly_Linked_List/Doubly_Linked_List.cpp
+++ Doubly_Linked_List/Doubly_Linked_List.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "DLL.h";
-
 using namespace std;
+
+struct some_object {
+    int field_1;
+    char field_2;
+};
 
 int main()
 {
-    cout << "Hello World!";
+    DLL::List<some_object>* ll = new DLL::List<some_object>();
+    some_object so;
+    so.field_1 = 123;
+    so.field_2 = 'a';
+    ll->add_at_head(so);
+    delete ll;
 }
